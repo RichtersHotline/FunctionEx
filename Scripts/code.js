18660470716cf90c91cbe022db389dd92683f5be
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-05-23 09:15:39 +0200

    exercise

---
+++ Scripts/code.js
@@ -0,0 +1,28 @@
+
+
+function greet(Hello) {
+
+console.log(Hello)
+
+}
+
+greet("Hello")
+
+function multiply(Num1, Num2) {
+
+console.log(Num1 * Num2)
+
+
+}
+
+multiply(2, 4)
+
+function tableOfFive(ParReturn) {
+
+let ConsoleReturn = ParReturn * 5
+
+console.log(ConsoleReturn)
+
+
+}
+tableOfFive(4)
